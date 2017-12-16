9a9f234866ed61bead2c7dbf389653c9fee16492
Marivaldo Sena <marivaldo.sena@live.com>
2017-12-16 21:44:44 -0200

    Add conditional statement to inddex and view files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
   before_action :find_item, only: %w[show edit update destroy]
-  before_action :authenticate_user!
 
   def index
-    @items = Item.all.order('created_at DESC')
+    if user_signed_in?
+      @items = Item.where(user_id: current_user.id).order('created_at DESC')
+    end
   end
 
   def show
